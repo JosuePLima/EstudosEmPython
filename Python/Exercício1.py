1c9084f92fcbdbb58e82b18039b1d7f0be2e901a
JosuePLima <paulolima899@gmail.com>
2023-03-21 11:56:06 -0300

    Python/

---
+++ Python/Exercício1.py
@@ -0,0 +1,7 @@
+# Lista de Exercícios 1
+# 1) Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+# input sempre devolve texto
+n1 = int(input("Primeiro número = "))
+n2 = int(input("Segundo número = "))
+print(n1 + n2)
+# Input sempre devolve texto
