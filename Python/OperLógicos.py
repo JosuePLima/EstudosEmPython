67a63756b79275e1482391aecae03639d13a404d
JosuePLima <paulolima899@gmail.com>
2023-03-31 20:56:36 -0300

    Operadores Lógicos

---
+++ Python/OperLógicos.py
@@ -0,0 +1,26 @@
+# Operadores Lógicos: AND, OR, NOT
+
+ling1 = "Python"
+ling2 = "Golang"
+
+if (ling1 == "Python" and ling2 == "R"):
+    print("Contratado!")
+else:
+    print("Estude python e golang")
+
+
+ling1 = "Python"
+ling2 = "R"
+
+if (ling1 == "Python" or ling2 == "Golang"):
+    print("Contratado!")
+else:
+    print("Estude python e golang")
+
+ling1 = "Python"
+ling2 = "Golang"
+
+if (ling1 == "Python" or ling2 == "R"):
+    print("Contratado!")
+else:
+    print("Estude python e golang")
